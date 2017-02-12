217e51647602735aed33827d018cf039474d3807
SA-LU <shl423@eng.ucsd.edu>
2017-02-11 17:41:02 -0800

    delete rmi_docker folder

--- lab1/project1/rmi_docker/server/IntPingPongServer.java
+++
@@ -1,6 +0,0 @@
-import rmi.server
-import rmi.RMIException
-
-public interface IntPingPongServer(args){
-	public String ping(int idNumber) throws RMIException;
-}
--- lab1/project1/rmi_docker/server/PingServerFactory.java
+++
@@ -1,8 +0,0 @@
-import rmi.server
-import rmi.RMIException
-
-public class PingServerFactory{
-	public static PingServer makePingServer(){
-		return new PingPongServer();
-	}
-}
